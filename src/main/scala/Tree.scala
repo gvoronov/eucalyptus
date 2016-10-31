0775522d0f4e9b296111f0852450ffc7584723e1
Gennady Voronov <gvoronov@gmail.com>
2016-10-30 23:09:58 -0600

    tree compiled

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,36 +1,70 @@
-package tree
+package eucalyptus.tree
 
-import datavalue._
+import scala.collection.mutable.{Map => MutableMap}
+
+import koalas.datavalue._
+import koalas.row.Row
+import koalas.series.Series
 
 sealed abstract class Node {
-  val isLeaf
+  val isLeaf: Boolean
   // def getChild(): Node
   // def setChild(): Unit
-  def predict(): DataValue
-  def getLeaf(): Leaf
+  def predict(row: Row): DataValue
+  def getLeaf(row: Row): Leaf
 }
 
-class BiNode(val feature: String, val split: Double, ) extends Node {
-  var children: Map[Boolean, Node] = Map.empty
+class BiNode(
+    val feature: String, val split: Double, val catMap: Map[DataValue, NumericalValue]=Map.empty)
+    extends Node {
+  protected var children: MutableMap[Boolean, Node] = MutableMap.empty
 
   val isLeaf = false
 
-  def getChild(key: Boolean): Node = children.get(key).getOrElse(new BiNode())
-  def setChild(key: Boolean, value: Node): Unit = {children(key) = value}
-  def predict(row): DataValue = {
-    val key: Boolean = row.select(column) match {
-      case value: NumericalValue => value() >= split
-      case value: SimpleCategoricalValue =>
-      case value: ClassCategoricalValue =>
-    }
+  def getChild(key: Boolean): Node = {
+    children
+      .get(key)
+      .getOrElse(throw new RuntimeException(
+        "This tree is malformed, it contains leafless branches"))
   }
-  def getLeaf(): Leaf
+  def getChild(row: Row): Node = {
+    val key: Boolean = row.select(feature) match {
+      case value: NumericalValue => value >= split
+      case value: CategoricalValue => catMap(value) >= split
+      case _ => throw new RuntimeException(
+        "feature " + feature + " of row contains value that can't be evaluate")
+    }
+    getChild(key)
+  }
+  def setChild(key: Boolean, node: Node): Unit = {children(key) = node}
+
+  def predict(row: Row): DataValue = getChild(row).predict(row)
+  def getLeaf(row: Row): Leaf = getChild(row).getLeaf(row)
 }
 
-class Leaf extends Node {
-  val isLeaf = true
+abstract class Leaf(responseSeries: Series[DataValue]) extends Node {
+  val prediction: DataValue
+
+  val isLeaf: Boolean = true
+  val numPoints: Int = responseSeries.length
   // def getChild(): Node
   // def setChild(): Unit
-  def predict(): DataValue
-  def getLeaf(): Leaf
+  def predict(row: Row): DataValue = prediction
+  def getLeaf(row: Row): Leaf = this
 }
+
+class RegressionLeaf(val responseSeries: Series[DataValue])
+    extends Leaf(responseSeries) {
+  val mean: NumericalValue = responseSeries.mean
+  val variance: NumericalValue = responseSeries.variance
+
+  override val prediction: NumericalValue = mean
+}
+
+// class ClassificationLeaf(val responseSeries: Series[CategoricalValue])
+//     extends Leaf[CategoricalValue](responseSeries) {
+  // val classCounts
+  // val classProbabilities
+  //
+  // override val prediction: CategoricalValue
+// }
---
+++ src/test/scala/TreeSuite.scala
@@ -0,0 +1,14 @@
+import org.scalatest.FunSuite
+
+import eucalyptus.tree._
+
+class TreeSuite extends FunSuite {
+
+  // test("get numerical value series and sum it") {
+  //   val atempSum1 = df.select[NumericalValue]("atemp").reduce((a, b) => a + b)
+  //   assert(atempSum1 ~= 8268.4955)
+  //
+  //   // val atempSum2 = df.select[NumericalValue]("atemp").sum
+  //   // assert(atempSum2 ~= 8268.4955)
+  // }
+}
