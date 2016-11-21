d384e1a8adca5ce6bffff025262216cb32df8134
Gennady Voronov <gvoronov@gmail.com>
2016-11-20 22:29:11 -0700

    minor change

--- src/main/scala/util/TreeUtil.scala
+++ src/main/scala/util/TreeUtil.scala
@@ -1,5 +1,11 @@
 package eucalyptus.util
 
 object TreeUtil{
-
+  /**
+   * Generate an equidepth histogram for input Series[NumericalValue]
+   * @param data
+   * @param numBins
+   * @param removeDups
+   */
+  def equidepthHist(data: Series[NumericalValue], numBins: Int, removeDups: Boolean = true) = {}
 }
